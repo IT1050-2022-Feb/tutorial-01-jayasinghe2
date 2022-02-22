6e80d640e629d86a4079de8583c6aa8da26ce8cc
jayasinghe <99546436+jayasinghe2@users.noreply.github.com>
2022-02-22 14:36:30 +0000

    tute01.c  it21366154

--- Tute01.c
+++ Tute01.c
@@ -5,6 +5,21 @@
 #include <stdio.h>
 
 int main() {
+
+  int sub_1,sub_2;
+  float cal,average;
+  
+  printf("subject_1: ");
+  scanf("%d",&sub_1);
+
+  printf("subject_2: ");
+  scanf("%d",&sub_2);
+
+  cal = sub_1 + sub_2;
+  average = cal/2;
+  
+  printf("calculate: %2f ",cal);
+  printf("\naverage: %2f",average);
   
   return 0;
 }
